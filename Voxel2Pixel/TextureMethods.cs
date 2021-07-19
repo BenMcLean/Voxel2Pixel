0d38b03bbd4d6c9a90f8d293c16ba49adeb8f71b
Benjamin McLean <mclean.ben@gmail.com>
2021-07-19 18:33:33 -0500

    RotateCounter135Thin

--- Voxel2Pixel/TextureMethods.cs
+++ Voxel2Pixel/TextureMethods.cs
@@ -321,7 +321,7 @@ namespace Voxel2Pixel
 		{
 			int xSide = (width < 1 ? (int)Math.Sqrt(texture.Length >> 2) : width) << 2,
 				ySide = (width < 1 ? xSide : texture.Length / width) >> 2,
-				newXside = (xSide + (ySide << 2));
+				newXside = xSide + (ySide << 2);
 			byte[] rotated = new byte[newXside * ((xSide >> 2) + ySide - 1)];
 			for (int y1 = texture.Length - 4, y2 = newXside * (xSide >> 2) - newXside; y1 > 0; y1 -= xSide, y2 += newXside + 4)
 				for (int x1 = y1, x2 = y2; x1 > y1 - xSide; x1 -= 4, x2 += -newXside + 4)
@@ -352,17 +352,37 @@ namespace Voxel2Pixel
 		/// <param name="texture">raw rgba8888 pixel data of source image</param>
 		/// <param name="width">width of texture or 0 to assume square texture</param>
 		/// <returns>new raw rgba8888 pixel data of newWidth = width + height - 1</returns>
-		public static byte[] RotateCounter135(this byte[] texture, int width = 0)
+		public static byte[] RotateCounter135Thin(this byte[] texture, int width = 0)
 		{
 			int xSide = (width < 1 ? (int)Math.Sqrt(texture.Length >> 2) : width) << 2,
 				ySide = (width < 1 ? xSide : texture.Length / width) >> 2,
-				newXside = (xSide + (ySide << 2)) - 4;
+				newXside = xSide + (ySide << 2) - 4;
 			byte[] rotated = new byte[newXside * ((xSide >> 2) + ySide)];
 			for (int y1 = xSide - 4, y2 = newXside * ySide - newXside; y1 >= 0; y1 -= 4, y2 += newXside + 4)
 				for (int x1 = y1, x2 = y2; x1 < texture.Length; x1 += xSide, x2 += -newXside + 4)
 				{
 					Array.Copy(texture, x1, rotated, x2, 4);
 					Array.Copy(texture, x1, rotated, x2 + newXside, 4);
+				}
+			return rotated;
+		}
+		/// <summary>
+		/// Rotates image counter-clockwise by 135 degrees
+		/// </summary>
+		/// <param name="texture">raw rgba8888 pixel data of source image</param>
+		/// <param name="width">width of texture or 0 to assume square texture</param>
+		/// <returns>new raw rgba8888 pixel data of newWidth = width + height</returns>
+		public static byte[] RotateCounter135(this byte[] texture, int width = 0)
+		{
+			int xSide = (width < 1 ? (int)Math.Sqrt(texture.Length >> 2) : width) << 2,
+				ySide = (width < 1 ? xSide : texture.Length / width) >> 2,
+				newXside = xSide + (ySide << 2);
+			byte[] rotated = new byte[newXside * ((xSide >> 2) + ySide - 1)];
+			for (int y1 = xSide - 4, y2 = newXside * ySide - newXside; y1 >= 0; y1 -= 4, y2 += newXside + 4)
+				for (int x1 = y1, x2 = y2; x1 < texture.Length; x1 += xSide, x2 += -newXside + 4)
+				{
+					Array.Copy(texture, x1, rotated, x2, 4);
+					Array.Copy(texture, x1, rotated, x2 + 4, 4);
 				}
 			return rotated;
 		}
