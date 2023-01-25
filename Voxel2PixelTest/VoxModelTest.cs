521c3283435edceb1e9cceb9c285dfe434ea0900
Benjamin McLean <mclean.ben@gmail.com>
2023-01-24 22:34:06 -0600

    Started working on VoxModelTest

---
+++ Voxel2PixelTest/VoxModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Voxel2PixelTest
+{
+	public class VoxModelTest
+	{
+		const string path = @"..\..\..\Sora.vox";
+		[Fact]
+		public void ArrayRendererTest()
+		{
+			if (!File.Exists(path))
+				throw new FileNotFoundException(Path.GetFullPath(path));
+		}
+	}
+}
