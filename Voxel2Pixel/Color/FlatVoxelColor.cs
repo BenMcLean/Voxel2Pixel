fe7dbb425545a7419b0868b6f8d1738c25016535
Benjamin McLean <mclean.ben@gmail.com>
2023-01-24 21:19:25 -0600

    Added FlatVoxelColor.cs

---
+++ Voxel2Pixel/Color/FlatVoxelColor.cs
@@ -0,0 +1,10 @@
+﻿namespace Voxel2Pixel.Color
+{
+	public class FlatVoxelColor : IVoxelColor
+	{
+		public int[] Palette { get; set; }
+		public int LeftFace(byte voxel) => Palette[voxel];
+		public int RightFace(byte voxel) => Palette[voxel];
+		public int VerticalFace(byte voxel) => Palette[voxel];
+	}
+}
