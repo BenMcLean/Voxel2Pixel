1e9676f0a5ab4a964b9064ff85b547dfb288f26f
Benjamin McLean <mclean.ben@gmail.com>
2023-01-24 18:36:52 -0600

    more draw methods

--- Voxel2Pixel/Draw/VoxelDraw.cs
+++ Voxel2Pixel/Draw/VoxelDraw.cs
@@ -8,9 +8,9 @@ namespace Voxel2Pixel.Draw
 		public static void Draw(IModel model, IRectangleRenderer renderer) => DrawRight(model, renderer);
 		public static void DrawRight(IModel model, IRectangleRenderer renderer)
 		{
-			for (int z = 0; z < model.Depth; z++)
-				for (int y = 0; y < model.Height; y++)
-					for (int x = 0; x < model.Width; x++)
+			for (int z = 0; z < model.SizeZ; z++)
+				for (int y = 0; y < model.SizeY; y++)
+					for (int x = 0; x < model.SizeX; x++)
 						if (model.At(x, y, z) is byte voxel
 							&& voxel != 0)
 						{
@@ -29,9 +29,9 @@ namespace Voxel2Pixel.Draw
 				scaleY: 6);
 		public static void DrawRightPeek(IModel model, IRectangleRenderer renderer, int scaleX, int scaleY)
 		{
-			for (int z = 0; z < model.Depth; z++)
-				for (int y = 0; y < model.Height; y++)
-					for (int x = 0; x < model.Width; x++)
+			for (int z = 0; z < model.SizeZ; z++)
+				for (int y = 0; y < model.SizeY; y++)
+					for (int x = 0; x < model.SizeX; x++)
 						if (model.At(x, y, z) is byte voxel
 							&& voxel != 0)
 						{
@@ -41,13 +41,82 @@ namespace Voxel2Pixel.Draw
 								voxel: voxel,
 								sizeX: scaleX,
 								sizeY: scaleY - 1);
-							if (z >= model.Depth - 1 || model.At(x, y, z + 1) == 0)
+							if (z >= model.SizeZ - 1 || model.At(x, y, z + 1) == 0)
 								renderer.RectVertical(
 									x: y * scaleX,
 									y: (z + 1) * scaleY - 1,
 									voxel: voxel,
 									sizeX: scaleX,
 									sizeY: 1);
+							break;
+						}
+		}
+		public static void DrawLeft(IModel model, IRectangleRenderer renderer)
+		{
+			for (int z = 0; z < model.SizeZ; z++)
+				for (int y = 0; y < model.SizeY; y++)
+					for (int x = model.SizeX - 1; x >= 0; x++)
+						if (model.At(x, y, z) is byte voxel
+							&& voxel != 0)
+						{
+							renderer.RectLeft(
+								x: y,
+								y: z,
+								voxel: voxel);
+							break;
+						}
+		}
+		public static void DrawLeftPeek(IModel model, IRectangleRenderer renderer, int scaleX, int scaleY)
+		{
+			for (int z = 0; z < model.SizeZ; z++)
+				for (int y = 0; y < model.SizeY; y++)
+					for (int x = model.SizeX - 1; x >= 0; x++)
+						if (model.At(x, y, z) is byte voxel
+							&& voxel != 0)
+						{
+							renderer.RectRight(
+								x: y * scaleX + 1,
+								y: z * scaleY,
+								voxel: voxel,
+								sizeX: scaleX,
+								sizeY: scaleY - 1);
+							if (z >= model.SizeZ - 1 || model.At(x, y, z + 1) == 0)
+								renderer.RectVertical(
+									x: y * scaleX,
+									y: (z + 1) * scaleY - 1,
+									voxel: voxel,
+									sizeX: scaleX,
+									sizeY: 1);
+							break;
+						}
+		}
+		public static void DrawTop(IModel model, IRectangleRenderer renderer)
+		{
+			for (int y = 0; y < model.SizeY; y++)
+				for (int x = 0; x < model.SizeX; x++)
+					for (int z = model.SizeZ - 1; z >= 0; z--)
+						if (model.At(x, y, z) is byte voxel
+							&& voxel != 0)
+						{
+							renderer.RectVertical(
+								x: y,
+								y: z,
+								voxel: voxel);
+							break;
+						}
+		}
+		public static void DrawBottom(IModel model, IRectangleRenderer renderer)
+		{
+			for (int y = 0; y < model.SizeY; y++)
+				for (int x = 0; x < model.SizeX; x++)
+					for (int z = 0; z < model.SizeZ; z--)
+						if (model.At(x, y, z) is byte voxel
+							&& voxel != 0)
+						{
+							renderer.RectVertical(
+								x: y,
+								y: z,
+								voxel: voxel);
 							break;
 						}
 		}
--- Voxel2Pixel/Model/IModel.cs
+++ Voxel2Pixel/Model/IModel.cs
@@ -2,18 +2,9 @@
 {
 	public interface IModel : IFetch
 	{
-		/// <summary>
-		/// Upper bound of x
-		/// </summary>
-		int Width { get; }
-		/// <summary>
-		/// Upper bound of y
-		/// </summary>
-		int Height { get; }
-		/// <summary>
-		/// Upper bound of z
-		/// </summary>
-		int Depth { get; }
+		int SizeX { get; }
+		int SizeY { get; }
+		int SizeZ { get; }
 		bool IsInside(int x, int y, int z);
 		bool IsOutside(int x, int y, int z);
 	}
