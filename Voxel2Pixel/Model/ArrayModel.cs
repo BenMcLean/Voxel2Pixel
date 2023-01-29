b6747ff75cafbc0e0e3a45206b4236df8f50e239
Benjamin McLean <mclean.ben@gmail.com>
2023-01-29 10:55:38 -0600

    Added ArrayModel

---
+++ Voxel2Pixel/Model/ArrayModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Voxel2Pixel.Model
+{
+	public class ArrayModel : IModel
+	{
+		public ArrayModel(byte[][][] model) => Model = model;
+		public ArrayModel(int sizeX = 1, int sizeY = 1, int sizeZ = 1)
+		{
+			Model = new byte[sizeX][][];
+			for (int x = 0; x < SizeX; x++)
+			{
+				Model[x] = new byte[sizeY][];
+				for (int y = 0; y < SizeY; y++)
+					Model[x][y] = new byte[sizeZ];
+			}
+		}
+		public byte[][][] Model;
+		#region IModel
+		public int SizeX => Model.Length;
+		public int SizeY => Model[0].Length;
+		public int SizeZ => Model[0][0].Length;
+		public byte? At(int x, int y, int z) => IsInside(x, y, z) ? Model[x][y][z] : (byte?)null;
+		public bool IsInside(int x, int y, int z) => !IsOutside(x, y, z);
+		public bool IsOutside(int x, int y, int z) => x < 0 || y < 0 || z < 0 || x >= SizeX || y >= SizeY || z >= SizeZ;
+		#endregion IModel
+	}
+}
