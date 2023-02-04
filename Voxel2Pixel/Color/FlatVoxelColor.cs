efa63fe2e932e304a990cd387660a3fb046f9c85
Benjamin McLean <mclean.ben@gmail.com>
2023-02-04 12:02:53 -0600

    testing uint colors

--- Voxel2Pixel/Color/FlatVoxelColor.cs
+++ Voxel2Pixel/Color/FlatVoxelColor.cs
@@ -2,9 +2,12 @@
 {
 	public class FlatVoxelColor : IVoxelColor
 	{
+		public FlatVoxelColor(uint[] palette) => Palette = palette;
 		public uint[] Palette { get; set; }
+		#region IVoxelColor
 		public uint LeftFace(byte voxel) => Palette[voxel];
 		public uint RightFace(byte voxel) => Palette[voxel];
 		public uint VerticalFace(byte voxel) => Palette[voxel];
+		#endregion IVoxelColor
 	}
 }
---
+++ Voxel2PixelTest/ColorTest.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Voxel2Pixel.Color;
+using Voxel2Pixel.Draw;
+using Voxel2Pixel.Model;
+using Voxel2Pixel.Render;
+using Xunit;
+
+namespace Voxel2PixelTest
+{
+	public class ColorTest
+	{
+		[Fact]
+		public void SideTest()
+		{
+			VoxModel model = new VoxModel(@"..\..\..\Sora.vox");
+			int width = VoxelDraw.DiagonalWidth(model),
+				height = VoxelDraw.DiagonalHeight(model);
+			ArrayRenderer arrayRenderer = new ArrayRenderer
+			{
+				Image = new byte[width * 4 * height],
+				Width = width,
+				IVoxelColor = new NaiveDimmer(model.Palette),
+			};
+			VoxelDraw.Diagonal(model, arrayRenderer);
+			ImageMaker.Png(
+				scaleX: 32,
+				scaleY: 32,
+				width: width,
+				bytes: arrayRenderer.Image)
+				.SaveAsPng("ColorTest.png");
+		}
+	}
+}
