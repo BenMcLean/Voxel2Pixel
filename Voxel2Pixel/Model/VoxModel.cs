312ad63fcf1dcfe14487c2db73e000f90a5e3344
Benjamin McLean <mclean.ben@gmail.com>
2023-02-12 13:40:47 -0600

    VoxModel should inherit from ArrayModel

--- Voxel2Pixel/Model/VoxModel.cs
+++ Voxel2Pixel/Model/VoxModel.cs
@@ -3,7 +3,7 @@ using Voxel2Pixel.Draw;
 
 namespace Voxel2Pixel.Model
 {
-	public class VoxModel : Voxel2Pixel.Model.IModel
+	public class VoxModel : ArrayModel
 	{
 		public VoxModel(VoxReader.Interfaces.IVoxFile voxFile, int modelNumber = 0)
 		{
@@ -30,13 +30,7 @@ namespace Voxel2Pixel.Model
 			set
 			{
 				model = value;
-				Voxels = new byte[SizeX][][];
-				for (int x = 0; x < Voxels.Length; x++)
-				{
-					Voxels[x] = new byte[SizeY][];
-					for (int y = 0; y < Voxels[x].Length; y++)
-						Voxels[x][y] = new byte[SizeZ];
-				}
+				Voxels = MakeModel(model.Size.X, model.Size.Y, model.Size.Z);
 				foreach (VoxReader.Voxel voxel in Model.Voxels)
 					if (Array.IndexOf(Palette, Color(voxel.Color)) is int index
 						&& index > 0
@@ -45,19 +39,10 @@ namespace Voxel2Pixel.Model
 			}
 		}
 		private VoxReader.Interfaces.IModel model;
-		public byte[][][] Voxels;
 		public static uint Color(VoxReader.Color color) => PixelDraw.Color(
 			r: color.R,
 			g: color.G,
 			b: color.B,
 			a: color.A);
-		#region IModel
-		public int SizeX => Model.Size.X;
-		public int SizeY => Model.Size.Y;
-		public int SizeZ => Model.Size.Z;
-		public byte? At(int x, int y, int z) => IsInside(x, y, z) ? Voxels[x][y][z] : (byte?)null;
-		public bool IsInside(int x, int y, int z) => !IsOutside(x, y, z);
-		public bool IsOutside(int x, int y, int z) => x < 0 || y < 0 || z < 0 || x >= SizeX || y >= SizeY || z >= SizeZ;
-		#endregion IModel
 	}
 }
