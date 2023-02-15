417a07fce0670fa92a974d797e8826a87a82e118
Benjamin McLean <mclean.ben@gmail.com>
2023-02-14 18:55:37 -0600

    slightly improved CubeRotationTest

--- Voxel2PixelTest/CubeRotationTest.cs
+++ Voxel2PixelTest/CubeRotationTest.cs
@@ -7,8 +7,8 @@ namespace Voxel2PixelTest
 {
 	public class CuberotationTest
 	{
-		private readonly ITestOutputHelper output;
-		public CuberotationTest(ITestOutputHelper output) => this.output = output;
+		//private readonly ITestOutputHelper output;
+		//public CuberotationTest(ITestOutputHelper output) => this.output = output;
 		public static readonly Matrix4x4 clockX = new Matrix4x4(
 				1, 0, 0, 0,
 				0, 0, -1, 0,
@@ -49,26 +49,38 @@ namespace Voxel2PixelTest
 			outY = (int)coords.M21;
 			outZ = (int)coords.M31;
 		}
+		[Fact]
+		public void MatrixTest()
+		{
+			Test24(-1, -2, -3);
+			Test24(1, -2, -3);
+			Test24(-1, 2, -3);
+			Test24(1, 2, -3);
+			Test24(-1, -2, 3);
+			Test24(1, -2, 3);
+			Test24(-1, 2, 3);
+			Test24(1, 2, 3);
+		}
 		private void TestRotation(int x, int y, int z, string name, CubeRotation cubeRotation, params Matrix4x4[] rotations)
 		{
 			cubeRotation.Rotate(out int x1, out int y1, out int z1, x, y, z);
 			Rotate(out int x2, out int y2, out int z2, x, y, z, rotations);
+			/*
 			output.WriteLine("Input: "
 				+ string.Join(", ", x, y, z)
 				+ ". " + cubeRotation.Name + ": "
 				+ string.Join(", ", x1, y1, z1)
 				+ ". Matrix4x4: "
 				+ string.Join(", ", x2, y2, z2) + ".");
+			*/
 			Assert.Equal(name, cubeRotation.Name);
 			Assert.Equal(x1, x2);
 			Assert.Equal(y1, y2);
 			Assert.Equal(z1, z2);
 		}
-		[Fact]
-		public void MatrixTest()
+		private void Test24(int x = 1, int y = 2, int z = 3)
 		{
 			CubeRotation c = CubeRotation.SOUTH0;
-			int x = 1, y = 2, z = 3;
 			TestRotation(x, y, z, "SOUTH0", c, Matrix4x4.Identity);
 			TestRotation(x, y, z, "SOUTH1", (CubeRotation)c.ClockY(), clockY);
 			TestRotation(x, y, z, "SOUTH2", (CubeRotation)c.ClockY().ClockY(), clockY, clockY);
