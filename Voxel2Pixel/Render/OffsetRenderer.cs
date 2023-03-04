7ffe7c31b98b310c55c78b7c701ab79daa73e130
Benjamin McLean <mclean.ben@gmail.com>
2023-03-04 14:22:49 -0600

    Added OffsetRenderer

---
+++ Voxel2Pixel/Render/OffsetRenderer.cs
@@ -0,0 +1,20 @@
+﻿namespace Voxel2Pixel.Render
+{
+	public class OffsetRenderer : TriangleRenderer
+	{
+		public IRectangleRenderer RectangleRenderer { get; set; }
+		public int OffsetX { get; set; } = 0;
+		public int OffsetY { get; set; } = 0;
+		public int ScaleX { get; set; } = 1;
+		public int ScaleY { get; set; } = 1;
+		#region IRectangleRenderer
+		public override void Rect(int x, int y, uint color, int sizeX = 1, int sizeY = 1) =>
+			RectangleRenderer.Rect(
+				x: x + OffsetX,
+				y: y + OffsetY,
+				color: color,
+				sizeX: sizeX * ScaleX,
+				sizeY: sizeY * ScaleY);
+		#endregion IRectangleRenderer
+	}
+}
