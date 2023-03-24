c850d4dc8ab4898369ce184d4e93c1e12155479e
Benjamin McLean <mclean.ben@gmail.com>
2023-03-23 23:28:49 -0500

    Pyramid2Test

--- Voxel2PixelTest/Pack8Test.cs
+++ Voxel2PixelTest/Pack8Test.cs
@@ -58,23 +58,47 @@ namespace Voxel2PixelTest
 			}
 			return voxels;
 		}
-		private static void Gif(IModel model, IVoxelColor voxelColor, string path)
+		[Fact]
+		public void Pyramid2Test() => Gif(
+			model: new ArrayModel(Pyramid2(17)),
+			voxelColor: new NaiveDimmer(ArrayModelTest.RainbowPalette),
+			path: "Pyramid2Test.gif",
+			originX: 0,
+			originY: 0,
+			originZ: 0);
+		public static byte[][][] Pyramid2(int width, params byte[] colors)
+		{
+			if (colors is null || colors.Length < 1)
+				colors = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+			int halfWidth = width >> 1;
+			byte[][][] voxels = ArrayModel.MakeModel(width, width, halfWidth + 1);
+			voxels[width - 1][0][0] = colors[2];
+			voxels[0][width - 1][0] = colors[3];
+			voxels[width - 1][width - 1][0] = colors[4];
+			for (int i = 0; i <= halfWidth; i++)
+				voxels[0][0][i] = colors[1];
+			return voxels;
+		}
+		private static void Gif(IModel model, IVoxelColor voxelColor, string path, int originX = -1, int originY = -1, int originZ = -1)
 		{
 			IsoPacker.IsoSprites(
 				model: model,
 				voxelColor: voxelColor,
 				sprites: out byte[][] sprites,
 				widths: out int[] widths,
-				origins: out int[][] origins);
-			int originX = origins.Select(origin => origin[0]).Max(),
-				originY = origins.Select(origin => origin[1]).Max() + 2,
+				origins: out int[][] origins,
+				originX: originX,
+				originY: originY,
+				originZ: originZ);
+			int pixelOriginX = origins.Select(origin => origin[0]).Max(),
+				pixelOriginY = origins.Select(origin => origin[1]).Max() + 2,
 				width = Enumerable.Range(0, sprites.Length)
 					.Select(i => widths[i]
-						+ originX - origins[i][0]
+						+ pixelOriginX - origins[i][0]
 						).Max(),
 				height = Enumerable.Range(0, sprites.Length)
 					.Select(i => PixelDraw.Height(sprites[i].Length, widths[i])
-						+ originY - origins[i][1]
+						+ pixelOriginY - origins[i][1]
 						).Max() + 1;
 			ImageMaker.AnimatedGifScaled(
 				scaleX: 4,
@@ -83,8 +107,8 @@ namespace Voxel2PixelTest
 				frames: Enumerable.Range(0, sprites.Length)
 					.Select(frame => new byte[width * 4 * height]
 						.DrawInsert(
-							x: originX - origins[frame][0],
-							y: originY - origins[frame][1],
+							x: pixelOriginX - origins[frame][0],
+							y: pixelOriginY - origins[frame][1],
 							insert: sprites[frame],
 							insertWidth: widths[frame],
 							width: width))
