49cc6a6adf844863ffa09bbbb663daa8d891e948
Benjamin McLean <mclean.ben@gmail.com>
2023-10-14 18:46:41 -0500

    Replaced VoxReader dependency with FileToVoxCore. Despite its total lack of documentation, FileToVoxCore is capable of both import and export of the vox file format while VoxReader can only import.

--- Voxel2Pixel/Model/VoxModel.cs
+++ Voxel2Pixel/Model/VoxModel.cs
@@ -1,45 +1,32 @@
 ﻿using System;
+using System.Linq;
 using Voxel2Pixel.Draw;
 
 namespace Voxel2Pixel.Model
 {
 	public class VoxModel : ArrayModel
 	{
-		public VoxModel(VoxReader.Interfaces.IVoxFile voxFile, int modelNumber = 0)
+		public VoxModel(FileToVoxCore.Vox.VoxModel model, int frame = 0)
 		{
-			VoxFile = voxFile;
-			Model = voxFile.Models[modelNumber];
+			uint[] palette = model.Palette.Take(255).Select(color => Color(color)).ToArray();
+			Palette = new uint[256];
+			Array.Copy(
+				sourceArray: palette,
+				sourceIndex: 0,
+				destinationArray: Palette,
+				destinationIndex: 1,
+				length: Math.Min(palette.Length, Palette.Length) - 1);
+			FileToVoxCore.Vox.VoxelData voxelData = model.VoxelFrames[frame];
+			Voxels = Bytes3D.Initialize<byte>(voxelData.VoxelsWide, voxelData.VoxelsTall, voxelData.VoxelsDeep);
+			for (int x = 0; x < SizeX; x++)
+				for (int y = 0; y < SizeY; y++)
+					for (int z = 0; z < SizeZ; z++)
+						if (voxelData.GetSafe(x, y, z) is byte voxel && voxel != 0)
+							Voxels[x][y][z] = voxel;
 		}
-		public VoxModel(string filePath, int modelNumber = 0) : this(VoxReader.VoxReader.Read(filePath), modelNumber) { }
-		public VoxReader.Interfaces.IVoxFile VoxFile
-		{
-			get => voxFile;
-			set
-			{
-				voxFile = value;
-				Palette = new uint[Math.Min(VoxFile.Palette.Colors.Length + 1, 256)];
-				for (int i = 0; i < Palette.Length - 1; i++)
-					Palette[i + 1] = Color(VoxFile.Palette.Colors[i]);
-			}
-		}
-		private VoxReader.Interfaces.IVoxFile voxFile;
+		public VoxModel(string filePath, int frame = 0) : this(new FileToVoxCore.Vox.VoxReader().LoadModel(filePath), frame) { }
 		public uint[] Palette;
-		public VoxReader.Interfaces.IModel Model
-		{
-			get => model;
-			set
-			{
-				model = value;
-				Voxels = Bytes3D.Initialize<byte>(model.Size.X, model.Size.Y, model.Size.Z);
-				foreach (VoxReader.Voxel voxel in Model.Voxels)
-					if (Array.IndexOf(Palette, Color(voxel.Color)) is int index
-						&& index > 0
-						&& index <= byte.MaxValue)
-						Voxels[voxel.Position.X][voxel.Position.Y][voxel.Position.Z] = (byte)index;
-			}
-		}
-		private VoxReader.Interfaces.IModel model;
-		public static uint Color(VoxReader.Color color) => PixelDraw.Color(
+		public static uint Color(FileToVoxCore.Drawing.Color color) => PixelDraw.Color(
 			r: color.R,
 			g: color.G,
 			b: color.B,
