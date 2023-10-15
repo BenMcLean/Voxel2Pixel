de02cb19277114141ee59ff9a98f77847956bdae
Benjamin McLean <mclean.ben@gmail.com>
2023-10-14 20:44:38 -0500

    Added code to write vox files using FileToVoxCore

--- Voxel2Pixel/Model/VoxModel.cs
+++ Voxel2Pixel/Model/VoxModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Voxel2Pixel.Draw;
 
@@ -9,8 +10,8 @@ namespace Voxel2Pixel.Model
 		#region Read
 		public VoxModel(FileToVoxCore.Vox.VoxModel model, int frame = 0)
 		{
-			uint[] palette = model.Palette.Take(256).Select(color => Color(color)).ToArray();
 			Palette = new uint[256];
+			uint[] palette = model.Palette.Take(Palette.Length).Select(color => Color(color)).ToArray();
 			Array.Copy(
 				sourceArray: palette,
 				sourceIndex: 0,
@@ -26,8 +27,32 @@ namespace Voxel2Pixel.Model
 							Voxels[x][y][z] = voxel;
 		}
 		public VoxModel(string filePath, int frame = 0) : this(new FileToVoxCore.Vox.VoxReader().LoadModel(filePath), frame) { }
-		public uint[] Palette;
+		public uint[] Palette { get; set; }
 		public static uint Color(FileToVoxCore.Drawing.Color color) => ((uint)color.ToArgb()).Argb2rgba();
 		#endregion Read
+		#region Write
+		public static FileToVoxCore.Drawing.Color Color(uint color) => FileToVoxCore.Drawing.Color.FromArgb(
+			alpha: color.A(),
+			red: color.R(),
+			green: color.G(),
+			blue: color.B());
+		public static IEnumerable<FileToVoxCore.Schematics.Voxel> FileToVoxCoreVoxels(IModel model, uint[] palette)
+		{
+			for (ushort x = 0; x < model.SizeX; x++)
+				for (ushort y = 0; y < model.SizeY; y++)
+					for (ushort z = 0; z < model.SizeZ; z++)
+						if (model.At(x, y, z) is byte voxel && voxel != 0)
+							yield return new FileToVoxCore.Schematics.Voxel(
+								x: x,
+								y: y,
+								z: z,
+								color: palette[voxel].Rgba2argb());
+		}
+		public static bool Write(string absolutePath, uint[] palette, IModel model) =>
+			new FileToVoxCore.Vox.VoxWriter().WriteModel(
+				absolutePath: absolutePath,
+				palette: palette.Skip(1).Select(@uint => Color(@uint)).ToList(),
+				schematic: new FileToVoxCore.Schematics.Schematic(FileToVoxCoreVoxels(model, palette).ToList()));
+		#endregion Write
 	}
 }
