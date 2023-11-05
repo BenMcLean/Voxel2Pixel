e2288654d515f08a2f0e8bf36951a9be00425dbc
Benjamin McLean <mclean.ben@gmail.com>
2023-11-04 19:45:34 -0500

    Deleted unused IFetch interface

--- Voxel2Pixel/Interfaces/IModel.cs
+++ Voxel2Pixel/Interfaces/IModel.cs
@@ -1,7 +1,8 @@
 ﻿namespace Voxel2Pixel.Interfaces
 {
-	public interface IModel : IFetch
+	public interface IModel
 	{
+		byte this[ushort x, ushort y, ushort z] { get; }
 		ushort SizeX { get; }
 		ushort SizeY { get; }
 		ushort SizeZ { get; }
