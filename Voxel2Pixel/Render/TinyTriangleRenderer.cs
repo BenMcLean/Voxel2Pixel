906f5b86646d9a878dfe8900a4685801abaef9a6
Benjamin McLean <mclean.ben@gmail.com>
2023-11-06 20:50:32 -0600

    I'm bad at math

--- Voxel2Pixel/Render/TinyTriangleRenderer.cs
+++ Voxel2Pixel/Render/TinyTriangleRenderer.cs
@@ -7,19 +7,18 @@ namespace Voxel2Pixel.Render
 	{
 		public virtual IRectangleRenderer RectangleRenderer { get; set; }
 		public virtual IVoxelColor VoxelColor { get; set; }
-		public static int IsoWidth(IModel model) => (2 * (model.SizeX + model.SizeY)) >> 1;
-		public static int IsoHeight(IModel model) => (2 * (model.SizeX + model.SizeY) + 4 * model.SizeZ - 1) >> 2;
+		public static int IsoWidth(IModel model) => model.SizeX + model.SizeY;
+		public static int IsoHeight(IModel model) => (model.SizeX + model.SizeY) / 2 + model.SizeZ - 1;
 		public static void IsoLocate(out int pixelX, out int pixelY, IModel model, int voxelX = 0, int voxelY = 0, int voxelZ = 0)
 		{
-			pixelX = (2 * (model.SizeY + voxelX - voxelY)) >> 1;
-			pixelY = (2 * (model.SizeX + model.SizeY) + 4 * model.SizeZ - 1 - 2 * (voxelX + voxelY) - 4 * voxelZ - 1) >> 2;
+			pixelX = model.SizeY + voxelX - voxelY;
+			pixelY = ((model.SizeX + model.SizeY - 1) + 2 * model.SizeZ - (voxelX + voxelY)) / 2 - voxelZ;
 		}
 		#region ITriangleRenderer
 		public virtual void Tri(int x, int y, bool right, uint color) =>
 			RectangleRenderer.Rect(
-				x: (x >> 1)//divided by 2
-					+ (right ? 0 : 1),
-				y: y >> 2,//divided by 4
+				x: x / 2 + (right ? 0 : 1),
+				y: y / 4,
 				color: color);
 		public virtual void Tri(int x, int y, bool right, byte voxel, VisibleFace visibleFace = VisibleFace.Front) => Tri(
 			x: x,
