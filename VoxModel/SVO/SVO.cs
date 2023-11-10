35210f884be334ea2de17f8e82cb788c06618988
Benjamin McLean <mclean.ben@gmail.com>
2023-11-09 20:15:03 -0600

    started working on my own custom Sparse Voxel Octree

---
+++ VoxModel/SVO/SVO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VoxModel.SVO
+{
+	public class SVO
+	{
+		public abstract class Node
+		{
+			public virtual byte Header { get; }
+			public virtual bool IsLeaf => (Header & 0b10000000) > 0;
+		}
+		public class Branch : Node
+		{
+			public override byte Header => (byte)(Children.Length - 1);
+			public Node[] Children;
+			public byte NumberOfChildren
+			{
+				get => (byte)Children.Length;
+				set => Children = new Node[Math.Min(value, (byte)8)];
+			}
+		}
+		public class Leaf : Node
+		{
+			public override byte Header => 0b10000000;
+			public ulong Data = 0;
+			public byte this[byte index]
+			{
+				get => (byte)(Data >> (index << 3));
+				set => Data = (Data & ~(0xFFul << (index << 3))) | ((ulong)value << (index << 3));
+			}
+			public byte this[bool x, bool y, bool z]
+			{
+				get => this[(byte)((z ? 4 : 0) + (y ? 2 : 0) + (x ? 1 : 0))];
+				set => this[(byte)((z ? 4 : 0) + (y ? 2 : 0) + (x ? 1 : 0))] = value;
+			}
+			public byte this[byte x, byte y, byte z]
+			{
+				get => this[(byte)((z > 0 ? 4 : 0) + (y > 0 ? 2 : 0) + (x > 0 ? 1 : 0))];
+				set => this[(byte)((z > 0 ? 4 : 0) + (y > 0 ? 2 : 0) + (x > 0 ? 1 : 0))] = value;
+			}
+		}
+	}
+}
---
+++ VoxModelTest/SVO/SvoTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Xunit;
+using static VoxModel.SVO.SVO;
+
+namespace VoxModelTest.SVO
+{
+	public class SvoTest
+	{
+		private readonly Xunit.Abstractions.ITestOutputHelper output;
+		public SvoTest(Xunit.Abstractions.ITestOutputHelper output) => this.output = output;
+
+		[Fact]
+		public void ByteTest()
+		{
+			byte[] bytes = Enumerable.Range(1, 8).Select(i => (byte)i).ToArray();
+			Leaf leaf = new Leaf();
+			for (byte i = 0; i < bytes.Length; i++)
+			{
+				output.WriteLine("before: " + string.Format("0x{0:X}", leaf.Data));
+				leaf[i] = bytes[i];
+				output.WriteLine("after: " + string.Format("0x{0:X}", leaf.Data));
+				output.WriteLine("bytes[" + i + "] = " + bytes[i] + ", leaf[" + i + "] = " + leaf[i]);
+			}
+			for (byte i = 0; i < bytes.Length; i++)
+				Assert.Equal(
+					expected: bytes[i],
+					actual: leaf[i]);
+		}
+	}
+}
