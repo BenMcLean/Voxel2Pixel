50a516a9644a94aa9ec52161737e47277a488dc4
Benjamin McLean <mclean.ben@gmail.com>
2023-11-23 21:53:42 -0600

    Added IndexedSprite2x.cs

--- Voxel2Pixel/Pack/IndexedSprite.cs
+++ Voxel2Pixel/Pack/IndexedSprite.cs
@@ -13,18 +13,18 @@ namespace Voxel2Pixel.Pack
 	public class IndexedSprite : ISprite, IRectangleRenderer, ITriangleRenderer, IVoxelColor
 	{
 		#region ISprite
-		public byte[] Texture => GetTexture();
-		public ushort Width => (ushort)Pixels.GetLength(0);
-		public ushort Height => (ushort)Pixels.GetLength(1);
-		public ushort OriginX { get; set; }
-		public ushort OriginY { get; set; }
+		public virtual byte[] Texture => GetTexture();
+		public virtual ushort Width => (ushort)Pixels.GetLength(0);
+		public virtual ushort Height => (ushort)Pixels.GetLength(1);
+		public virtual ushort OriginX { get; set; }
+		public virtual ushort OriginY { get; set; }
 		#endregion ISprite
 		#region IndexedSprite
 		/// <summary>
 		/// Each pixel is an index corresponding to a color in Palette.
 		/// x+ is right, y+ is down.
 		/// </summary>
-		public byte[,] Pixels { get; set; }
+		public virtual byte[,] Pixels { get; set; }
 		/// <summary>
 		/// Expected to be length 256 of Big Endian RGBA8888 32-bit colors.
 		/// 0 is the transparent color.
@@ -34,9 +34,9 @@ namespace Voxel2Pixel.Pack
 		/// 193-255 are for Right face colors.
 		/// 64, 128 and 192 are unused.
 		/// </summary>
-		public uint[] Palette { get; set; }
-		public byte[] GetTexture(bool transparent0 = true) => GetTexture(Pixels, Palette, transparent0);
-		public byte[] GetTexture(uint[] palette, bool transparent0 = true) => GetTexture(Pixels, palette ?? Palette, transparent0);
+		public virtual uint[] Palette { get; set; }
+		public virtual byte[] GetTexture(bool transparent0 = true) => GetTexture(Pixels, Palette, transparent0);
+		public virtual byte[] GetTexture(uint[] palette, bool transparent0 = true) => GetTexture(Pixels, palette ?? Palette, transparent0);
 		public static byte[] GetTexture(byte[,] pixels, uint[] palette, bool transparent0 = true)
 		{
 			ushort width = (ushort)pixels.GetLength(0),
@@ -56,15 +56,15 @@ namespace Voxel2Pixel.Pack
 		}
 		#endregion IndexedSprite
 		#region IRectangleRenderer
-		public void Rect(ushort x, ushort y, uint color, ushort sizeX = 1, ushort sizeY = 1)
+		public virtual void Rect(ushort x, ushort y, uint color, ushort sizeX = 1, ushort sizeY = 1)
 		{
 			if (Array.IndexOf(Palette, color) is int index && index >= 0)
 				Rect(x, y, (byte)index, sizeX, sizeY);
 			else
 				throw new KeyNotFoundException(color.ToString("X"));
 		}
-		public void Rect(ushort x, ushort y, byte index, VisibleFace visibleFace, ushort sizeX = 1, ushort sizeY = 1) => Rect(x, y, Index(index, visibleFace), sizeX, sizeY);
-		public void Rect(ushort x, ushort y, byte index, ushort sizeX = 1, ushort sizeY = 1)
+		public virtual void Rect(ushort x, ushort y, byte index, VisibleFace visibleFace, ushort sizeX = 1, ushort sizeY = 1) => Rect(x, y, Index(index, visibleFace), sizeX, sizeY);
+		public virtual void Rect(ushort x, ushort y, byte index, ushort sizeX = 1, ushort sizeY = 1)
 		{
 			sizeX = Math.Min((ushort)(x + sizeX), Width);
 			sizeY = Math.Min((ushort)(y + sizeY), Height);
@@ -74,47 +74,31 @@ namespace Voxel2Pixel.Pack
 		}
 		#endregion IRectangleRenderer
 		#region ITriangleRenderer
-		public void Tri(ushort x, ushort y, bool right, uint color)
+		public virtual void Tri(ushort x, ushort y, bool right, uint color)
 		{
 			if (Array.IndexOf(Palette, color) is int index && index >= 0)
 				Tri(x, y, right, (byte)index);
 			else
 				throw new KeyNotFoundException(color.ToString("X"));
 		}
-		public void Tri(ushort x, ushort y, bool right, byte index, VisibleFace visibleFace) => Tri(x, y, right, Index(index, visibleFace));
-		public void Tri(ushort x, ushort y, bool right, byte index)
+		public virtual void Tri(ushort x, ushort y, bool right, byte index, VisibleFace visibleFace) => Tri(x, y, right, Index(index, visibleFace));
+		public virtual void Tri(ushort x, ushort y, bool right, byte index)
 		{
-			if (right)
-			{
-				Rect(
-					x: x,
-					y: y,
-					index: index,
-					sizeX: 1,
-					sizeY: 3);
-				Rect(
-					x: (ushort)(x + 1),
-					y: (ushort)(y + 1),
-					index: index);
-			}
-			else
-			{
-				Rect(
-					x: (ushort)(x + 1),
-					y: y,
-					index: index,
-					sizeX: 1,
-					sizeY: 3);
-				Rect(
-					x: x,
-					y: (ushort)(y + 1),
-					index: index);
-			}
+			Rect(
+				x: (ushort)(x + (right ? 0 : 1)),
+				y: y,
+				index: index,
+				sizeX: 1,
+				sizeY: 3);
+			Rect(
+				x: (ushort)(x + (right ? 1 : 0)),
+				y: (ushort)(y + 1),
+				index: index);
 		}
 		#endregion ITriangleRenderer
 		#region IVoxelColor
 		public static byte Index(byte voxel, VisibleFace visibleFace = VisibleFace.Front) => (byte)(voxel < 64 ? (byte)visibleFace + voxel : voxel);
-		public uint this[byte index, VisibleFace visibleFace = VisibleFace.Front] => Palette[Index(index, visibleFace)];
+		public virtual uint this[byte index, VisibleFace visibleFace = VisibleFace.Front] => Palette[Index(index, visibleFace)];
 		#endregion IVoxelColor
 	}
 }
---
+++ Voxel2Pixel/Pack/IndexedSprite2x.cs
@@ -0,0 +1,23 @@
+﻿namespace Voxel2Pixel.Pack
+{
+	/// <summary>
+	/// IndexedSprite, except that triangles are drawn at 2x horizontal scale.
+	/// </summary>
+	public class IndexedSprite2x : IndexedSprite
+	{
+		public override void Tri(ushort x, ushort y, bool right, byte index)
+		{
+			Rect(
+				x: (ushort)((x + (right ? 0 : 1)) << 1),
+				y: y,
+				index: index,
+				sizeX: 2,
+				sizeY: 3);
+			Rect(
+				x: (ushort)((x + (right ? 1 : 0)) << 1),
+				y: (ushort)(y + 1),
+				index: index,
+				sizeX: 2);
+		}
+	}
+}
