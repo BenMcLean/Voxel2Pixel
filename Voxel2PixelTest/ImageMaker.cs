8856a03c88aa4fe2d5161593dfe6de55584efc23
Benjamin McLean <mclean.ben@gmail.com>
2023-11-25 16:08:48 -0600

    more work on Voxel2PixelTest.ImageMaker

--- Voxel2PixelTest/ImageMaker.cs
+++ Voxel2PixelTest/ImageMaker.cs
@@ -5,9 +5,9 @@ using static Voxel2Pixel.Draw.PixelDraw;
 using static Voxel2Pixel.Draw.DrawFont3x4;
 using System.Collections.ObjectModel;
 using Voxel2Pixel.Model;
-using System.Collections;
 using Voxel2Pixel.Pack;
 using System.Collections.Generic;
+using Voxel2Pixel.Interfaces;
 
 namespace Voxel2PixelTest
 {
@@ -24,6 +24,7 @@ namespace Voxel2PixelTest
 					color: color);
 			return frames;
 		}
+		#region SixLabors.ImageSharp
 		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> Png(int width = 0, params byte[] bytes)
 		{
 			if (width < 1)
@@ -33,32 +34,21 @@ namespace Voxel2PixelTest
 				width: width,
 				height: bytes.Length / width >> 2);
 		}
-		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> Png(ushort scaleX, ushort scaleY, ushort width = 0, params byte[] bytes)
-		{
-			if (scaleX == 1 && scaleY == 1)
-				return Png(width, bytes);
-			if (width < 1)
-				width = (ushort)Math.Sqrt(bytes.Length >> 2);
-			return Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(
-				data: bytes.Upscale(scaleX, scaleY, width),
-				width: width * scaleX,
-				height: (bytes.Length / width >> 2) * scaleY);
-		}
-		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> AnimatedGif(int frameDelay = 25, ushort repeatCount = 0, params Sprite[] sprites) => AnimatedGif(sprites.AsEnumerable(), frameDelay, repeatCount);
-		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> AnimatedGif(IEnumerable<Sprite> sprites, int frameDelay = 25, ushort repeatCount = 0)
+		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> Png(this ISprite sprite) => SixLabors.ImageSharp.Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(
+			data: sprite.Texture,
+			width: sprite.Width,
+			height: sprite.Height);
+		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> AnimatedGif(int frameDelay = 25, ushort repeatCount = 0, params ISprite[] sprites) => sprites.AsEnumerable().AnimatedGif(frameDelay, repeatCount);
+		public static SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> AnimatedGif(this IEnumerable<ISprite> sprites, int frameDelay = 25, ushort repeatCount = 0)
 		{
 			Sprite[] resized = Sprite.SameSize(sprites).ToArray();
-			Sprite first = resized.First();
-			SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> gif = new SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32>(first.Width, first.Height);
+			SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> gif = new SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32>(resized[0].Width, resized[0].Height);
 			SixLabors.ImageSharp.Formats.Gif.GifMetadata gifMetaData = gif.Metadata.GetGifMetadata();
 			gifMetaData.RepeatCount = repeatCount;
 			gifMetaData.ColorTableMode = SixLabors.ImageSharp.Formats.Gif.GifColorTableMode.Local;
 			foreach (Sprite sprite in resized)
 			{
-				SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> image = SixLabors.ImageSharp.Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(
-					data: sprite.Texture,
-					width: sprite.Width,
-					height: sprite.Height);
+				SixLabors.ImageSharp.Image<SixLabors.ImageSharp.PixelFormats.Rgba32> image = sprite.Png();
 				SixLabors.ImageSharp.Formats.Gif.GifFrameMetadata metadata = image.Frames.RootFrame.Metadata.GetGifMetadata();
 				metadata.FrameDelay = frameDelay;
 				metadata.DisposalMethod = SixLabors.ImageSharp.Formats.Gif.GifDisposalMethod.RestoreToBackground;
@@ -98,6 +88,7 @@ namespace Voxel2PixelTest
 			gif.Frames.RemoveFrame(0);//I don't know why ImageSharp has me doing this but if I don't then I get an extra transparent frame at the start.
 			return gif;
 		}
+		#endregion SixLabors.ImageSharp
 		#region Test data
 		public static readonly ReadOnlyCollection<uint> Rainbow = Array.AsReadOnly(new uint[7]
 		{//Just a color test, not a political statement.
