0b01d8a85b4209c6e6e36a75c39c4f721e5df28a
Benjamin McLean <mclean.ben@gmail.com>
2023-11-25 12:51:48 -0600

    Eliminated lots of obsolete code

--- Voxel2PixelTest/ImageMaker.cs
+++ Voxel2PixelTest/ImageMaker.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using static Voxel2Pixel.Draw.PixelDraw;
 using static Voxel2Pixel.Draw.DrawFont3x4;
+using System.Collections.ObjectModel;
+using Voxel2Pixel.Model;
 
 namespace Voxel2PixelTest
 {
@@ -70,5 +72,127 @@ namespace Voxel2PixelTest
 			gif.Frames.RemoveFrame(0); // I don't know why ImageSharp has me doing this but if I don't then I get an extra transparent frame at the start.
 			return gif;
 		}
+		#region Test data
+		public static readonly ReadOnlyCollection<uint> Rainbow = Array.AsReadOnly(new uint[7]
+		{//Just a color test, not a political statement.
+			0xFF0000FFu,//Red
+			0xFFA500FFu,//Orange
+			0xFFFF00FFu,//Yellow
+			0x00FF00FFu,//Green
+			0x0000FFFFu,//Blue
+			0x4B0082FFu,//Indigo
+			0x8F00FFFFu//Violet
+		});
+		public static readonly uint[] RainbowPalette =
+			Enumerable.Range(0, byte.MaxValue)
+			.Select(i => i == 0 ? 0 : Rainbow[(i - 1) % Rainbow.Count])
+			.ToArray();
+		public static byte[][][] RainbowBox(int sizeX, int sizeY, int sizeZ)
+		{
+			byte[][][] model = Array3D.Initialize<byte>(sizeX, sizeY, sizeZ);
+			for (int x = 0; x < sizeX; x++)
+			{
+				byte voxel = (byte)((sizeX - 1 - x) % Rainbow.Count + 1);
+				model[x][0][0] = voxel;
+				model[x][sizeY - 1][0] = voxel;
+				model[x][0][sizeZ - 1] = voxel;
+				model[x][sizeY - 1][sizeZ - 1] = voxel;
+			}
+			for (int y = 1; y < sizeY - 1; y++)
+			{
+				byte voxel = (byte)((sizeY - 1 - y) % Rainbow.Count + 1);
+				model[0][y][0] = voxel;
+				model[sizeX - 1][y][0] = voxel;
+				model[0][y][sizeZ - 1] = voxel;
+				model[sizeX - 1][y][sizeZ - 1] = voxel;
+			}
+			for (int z = 1; z < sizeZ - 1; z++)
+			{
+				byte voxel = (byte)((sizeZ - 1 - z) % Rainbow.Count + 1);
+				model[0][0][z] = voxel;
+				model[sizeX - 1][0][z] = voxel;
+				model[0][sizeY - 1][z] = voxel;
+				model[sizeX - 1][sizeY - 1][z] = voxel;
+			}
+			return model;
+		}
+		public static byte[][][] AltRainbowBox(int sizeX, int sizeY, int sizeZ)
+		{
+			byte[][][] model = Array3D.Initialize<byte>(sizeX, sizeY, sizeZ);
+			model[0][3][0] = 1;
+			model[3][0][0] = 1;
+			for (int x = 1; x < sizeX; x++)
+			{
+				byte voxel = (byte)((sizeX - 1 - x) % Rainbow.Count + 1);
+				model[x][1][0] = voxel;
+				model[x][sizeY - 1][0] = voxel;
+				model[x][1][sizeZ - 1] = voxel;
+				model[x][sizeY - 1][sizeZ - 1] = voxel;
+			}
+			for (int y = 1; y < sizeY - 1; y++)
+			{
+				byte voxel = (byte)((sizeY - 1 - y) % Rainbow.Count + 1);
+				model[1][y][0] = voxel;
+				model[sizeX - 1][y][0] = voxel;
+				model[1][y][sizeZ - 1] = voxel;
+				model[sizeX - 1][y][sizeZ - 1] = voxel;
+			}
+			for (int z = 1; z < sizeZ - 1; z++)
+			{
+				byte voxel = (byte)((sizeZ - 1 - z) % Rainbow.Count + 1);
+				model[1][1][z] = voxel;
+				model[sizeX - 1][1][z] = voxel;
+				model[1][sizeY - 1][z] = voxel;
+				model[sizeX - 1][sizeY - 1][z] = voxel;
+			}
+			return model;
+		}
+		public static byte[][][] SmallerRainbowBox(int sizeX, int sizeY, int sizeZ)
+		{
+			byte[][][] model = Array3D.Initialize<byte>(sizeX, sizeY, sizeZ);
+			for (int x = 1; x < sizeX - 1; x++)
+			{
+				byte voxel = (byte)(x % Rainbow.Count);
+				model[x][1][1] = voxel;
+				model[x][sizeY - 2][1] = voxel;
+				model[x][1][sizeZ - 2] = voxel;
+				model[x][sizeY - 2][sizeZ - 2] = voxel;
+			}
+			for (int y = 1; y < sizeY - 2; y++)
+			{
+				byte voxel = (byte)(y % Rainbow.Count);
+				model[1][y][1] = voxel;
+				model[sizeX - 2][y][1] = voxel;
+				model[1][y][sizeZ - 2] = voxel;
+				model[sizeX - 2][y][sizeZ - 2] = voxel;
+			}
+			for (int z = 1; z < sizeZ - 2; z++)
+			{
+				byte voxel = (byte)(z % Rainbow.Count);
+				model[1][1][z] = voxel;
+				model[sizeX - 2][1][z] = voxel;
+				model[1][sizeY - 2][z] = voxel;
+				model[sizeX - 2][sizeY - 2][z] = voxel;
+			}
+			return model;
+		}
+		public static byte[] TestTexture(ushort width, ushort height) =>
+			new byte[16] {
+				255,0,0,255,
+				0,255,0,255,
+				0,0,255,255,
+				128,128,128,255}
+			.Upscale(
+				factorX: (ushort)(width >> 1),
+				factorY: (ushort)(height >> 1),
+				width: 2)
+			.DrawRectangle(
+				color: 0xFFu,
+				x: width >> 2,
+				y: height >> 2,
+				rectWidth: width >> 1,
+				rectHeight: height >> 1,
+				width: width);
+		#endregion Test data
 	}
 }
