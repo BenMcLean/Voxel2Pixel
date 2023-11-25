281e39a07c52c7d653a525e6d7266d1250fabfee
Benjamin McLean <mclean.ben@gmail.com>
2023-11-25 11:33:43 -0600

    More work on Sprite class

--- Voxel2Pixel/Pack/Sprite.cs
+++ Voxel2Pixel/Pack/Sprite.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using RectpackSharp;
+using System.Collections.Generic;
 using System.Linq;
 using Voxel2Pixel.Draw;
 using Voxel2Pixel.Interfaces;
 using Voxel2Pixel.Model;
+using Voxel2Pixel.Render;
 
 namespace Voxel2Pixel.Pack
 {
@@ -85,6 +87,29 @@ namespace Voxel2Pixel.Pack
 			right: right,
 			color: this[index, visibleFace]);
 		#endregion ITriangleRenderer
+		#region Image manipulation
+		public Sprite(out RectpackSharp.PackingRectangle[] packingRectangles, IEnumerable<ISprite> sprites) : this(out packingRectangles, sprites.ToArray()) { }
+		public Sprite(out RectpackSharp.PackingRectangle[] packingRectangles, params ISprite[] sprites)
+		{
+			packingRectangles = Enumerable.Range(0, sprites.Length)
+				.Select(i => new PackingRectangle(
+					x: 0,
+					y: 0,
+					width: (ushort)(sprites[i].Width + 2),
+					height: (ushort)(sprites[i].Height + 2),
+					id: i))
+				.ToArray();
+			RectanglePacker.Pack(packingRectangles, out PackingRectangle bounds, PackingHints.TryByBiggerSide);
+			Width = (ushort)PixelDraw.NextPowerOf2(bounds.BiggerSide);
+			Texture = new byte[Width * Width << 2];
+			foreach (PackingRectangle packingRectangle in packingRectangles)
+				Texture.DrawInsert(
+					x: (ushort)(packingRectangle.X + 1),
+					y: (ushort)(packingRectangle.Y + 1),
+					insert: sprites[packingRectangle.Id].Texture,
+					insertWidth: sprites[packingRectangle.Id].Width,
+					width: Width);
+		}
 		public static IEnumerable<Sprite> SameSize(ushort addWidth, ushort addHeight, params ISprite[] sprites) => SameSize(addWidth, addHeight, sprites.AsEnumerable());
 		public static IEnumerable<Sprite> SameSize(params ISprite[] sprites) => SameSize(0, 0, sprites);
 		public static IEnumerable<Sprite> SameSize(IEnumerable<ISprite> sprites) => SameSize(0, 0, sprites);
@@ -136,5 +161,147 @@ namespace Voxel2Pixel.Pack
 			OriginX = (ushort)(OriginX * factorX),
 			OriginY = (ushort)(OriginY * factorY),
 		};
+		#endregion Image manipulation
+		#region Voxel drawing
+		public static IEnumerable<Sprite> Above4(IModel model, IVoxelColor voxelColor, params ushort[] voxelOrigin)
+		{
+			if (voxelOrigin is null || voxelOrigin.Length < 3)
+				voxelOrigin = new ushort[3];
+			TurnModel turnModel = new TurnModel
+			{
+				Model = model,
+			};
+			for (byte angle = 0; angle < 4; angle++)
+			{
+				turnModel.ReverseRotate(
+					x: out ushort turnedX,
+					y: out ushort turnedY,
+					z: out ushort turnedZ,
+					coordinates: voxelOrigin);
+				VoxelDraw.AboveLocate(
+					pixelX: out int locateX,
+					pixelY: out int locateY,
+					model: turnModel,
+					voxelX: turnedX,
+					voxelY: turnedY,
+					voxelZ: turnedZ);
+				ushort width = VoxelDraw.AboveWidth(turnModel);
+				Sprite sprite = new Sprite
+				{
+					Texture = new byte[width * VoxelDraw.AboveHeight(turnModel) << 2],
+					Width = width,
+					VoxelColor = voxelColor,
+					OriginX = (ushort)locateX,
+					OriginY = (ushort)locateY,
+				};
+				VoxelDraw.Above(
+					model: turnModel,
+					renderer: sprite);
+				yield return sprite;
+				turnModel.ClockZ();
+			}
+		}
+		public static IEnumerable<Sprite> Iso4(IModel model, IVoxelColor voxelColor, params ushort[] voxelOrigin)
+		{
+			if (voxelOrigin is null || voxelOrigin.Length < 3)
+				voxelOrigin = new ushort[3];
+			TurnModel turnModel = new TurnModel
+			{
+				Model = model,
+			};
+			for (byte angle = 0; angle < 4; angle++)
+			{
+				turnModel.ReverseRotate(
+					x: out ushort turnedX,
+					y: out ushort turnedY,
+					z: out ushort turnedZ,
+					coordinates: voxelOrigin);
+				VoxelDraw.IsoLocate(
+					pixelX: out int locateX,
+					pixelY: out int locateY,
+					model: turnModel,
+					voxelX: turnedX,
+					voxelY: turnedY,
+					voxelZ: turnedZ);
+				ushort width = VoxelDraw.IsoWidth(turnModel);
+				Sprite sprite = new Sprite
+				{
+					Texture = new byte[width * VoxelDraw.IsoHeight(turnModel) << 2],
+					Width = width,
+					VoxelColor = voxelColor,
+					OriginX = (ushort)locateX,
+					OriginY = (ushort)locateY,
+				};
+				VoxelDraw.Iso(
+					model: turnModel,
+					renderer: sprite);
+				yield return sprite;
+				turnModel.ClockZ();
+			}
+		}
+		public static IEnumerable<Sprite> Iso8(IModel model, IVoxelColor voxelColor, params ushort[] voxelOrigin)
+		{
+			if (voxelOrigin is null || voxelOrigin.Length < 3)
+				voxelOrigin = new ushort[3];
+			TurnModel turnModel = new TurnModel
+			{
+				Model = model,
+			};
+			for (byte angle = 0; angle < 4; angle++)
+			{
+				turnModel.ReverseRotate(
+					x: out ushort turnedX,
+					y: out ushort turnedY,
+					z: out ushort turnedZ,
+					coordinates: voxelOrigin);
+				VoxelDraw.AboveLocate(
+					pixelX: out int locateX,
+					pixelY: out int locateY,
+					model: turnModel,
+					voxelX: turnedX,
+					voxelY: turnedY,
+					voxelZ: turnedZ);
+				ushort width = VoxelDraw.AboveWidth(turnModel);
+				Sprite sprite = new Sprite
+				{
+					Texture = new byte[width * VoxelDraw.AboveHeight(turnModel) << 2],
+					Width = width,
+					VoxelColor = voxelColor,
+					OriginX = (ushort)locateX,
+					OriginY = (ushort)locateY,
+				};
+				VoxelDraw.Above(
+					model: turnModel,
+					renderer: sprite);
+				yield return sprite;
+				turnModel.ClockZ();
+				turnModel.ReverseRotate(
+					x: out turnedX,
+					y: out turnedY,
+					z: out turnedZ,
+					coordinates: voxelOrigin);
+				VoxelDraw.IsoLocate(
+					pixelX: out locateX,
+					pixelY: out locateY,
+					model: turnModel,
+					voxelX: turnedX,
+					voxelY: turnedY,
+					voxelZ: turnedZ);
+				width = VoxelDraw.IsoWidth(turnModel);
+				sprite = new Sprite
+				{
+					Texture = new byte[width * VoxelDraw.IsoHeight(turnModel) << 2],
+					Width = width,
+					VoxelColor = voxelColor,
+					OriginX = (ushort)locateX,
+					OriginY = (ushort)locateY,
+				};
+				VoxelDraw.Iso(
+					model: turnModel,
+					renderer: sprite);
+				yield return sprite;
+			}
+		}
+		#endregion Voxel drawing
 	}
 }
