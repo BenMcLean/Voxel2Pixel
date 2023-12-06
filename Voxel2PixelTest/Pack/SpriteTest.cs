4057b6a5531b69f4bcc84290c8e88a9b13257993
Benjamin McLean <mclean.ben@gmail.com>
2023-12-05 22:55:10 -0600

    testing new stupid, lazy "BenSprite" image file format.

--- Voxel2PixelTest/Pack/SpriteTest.cs
+++ Voxel2PixelTest/Pack/SpriteTest.cs
@@ -5,6 +5,8 @@ using Voxel2Pixel.Pack;
 using static Voxel2Pixel.ExtensionMethods;
 using Xunit;
 using System.Linq;
+using Voxel2Pixel.Draw;
+using System.IO;
 
 namespace Voxel2PixelTest.Pack
 {
@@ -23,6 +25,31 @@ namespace Voxel2PixelTest.Pack
 				.Select(sprite => sprite.Upscale(8, 8))
 				.AnimatedGif(frameDelay: 100)
 				.SaveAsGif("Sora.gif");
+		}
+		[Fact]
+		public void BenSprite()
+		{
+			VoxFileModel model = new VoxFileModel(@"..\..\..\Sora.vox");
+			IndexedSprite2x sprite = new IndexedSprite2x(
+				width: (ushort)(VoxelDraw.IsoWidth(model) << 1),
+				height: VoxelDraw.IsoHeight(model))
+			{
+				Palette = new NaiveDimmer(model.Palette).CreatePalette(),
+			};
+			VoxelDraw.Iso(model, sprite);
+			string file = "Sora.BenSprite";
+			if (File.Exists(file))
+				File.Delete(file);
+			using (FileStream fs = new FileStream(
+				path: file,
+				mode: FileMode.CreateNew))
+				sprite.Write(fs);
+			using (FileStream fs = new FileStream(
+				path: file,
+				mode: FileMode.Open))
+				new IndexedSprite(fs)
+					.Png()
+					.SaveAsPng("BenSprite.png");
 		}
 	}
 }
