4083afc4e1f2b477cdbbd603dd76229d263856aa
Benjamin McLean <mclean.ben@gmail.com>
2024-03-14 21:29:24 -0500

    Update to Voxel2Pixel/Color/FlatVoxelColor.cs

--- Voxel2Pixel/Color/FlatVoxelColor.cs
+++ Voxel2Pixel/Color/FlatVoxelColor.cs
@@ -3,10 +3,9 @@ using Voxel2Pixel.Model;
 
 namespace Voxel2Pixel.Color
 {
-	public class FlatVoxelColor : IVoxelColor
+	public class FlatVoxelColor(uint[] palette) : IVoxelColor
 	{
-		public FlatVoxelColor(uint[] palette) => Palette = palette;
-		public uint[] Palette { get; set; }
+		public uint[] Palette { get; set; } = palette;
 		#region IVoxelColor
 		public uint this[byte voxel, VisibleFace visibleFace = VisibleFace.Front] => Palette[voxel];
 		#endregion IVoxelColor
