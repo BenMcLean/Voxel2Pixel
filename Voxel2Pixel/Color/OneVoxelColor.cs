24f3f513ba1a6b28cc50b9694e7aaa92929243b2
Benjamin McLean <mclean.ben@gmail.com>
2024-03-14 21:11:39 -0500

    Added OneVoxelColor class

---
+++ Voxel2Pixel/Color/OneVoxelColor.cs
@@ -0,0 +1,13 @@
+﻿using Voxel2Pixel.Interfaces;
+using Voxel2Pixel.Model;
+
+namespace Voxel2Pixel.Color
+{
+	public class OneVoxelColor : IVoxelColor
+	{
+		public uint Color { get; set; } = 0x7Fu;
+		#region IVoxelColor
+		public uint this[byte index, VisibleFace visibleFace = VisibleFace.Top] => Color;
+		#endregion IVoxelColor
+	}
+}
