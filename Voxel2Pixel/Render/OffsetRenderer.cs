ea12c240da7e03dfab347c0b93129721db626ef4
Benjamin McLean <mclean.ben@gmail.com>
2024-03-14 19:16:17 -0500

    Better handling of negative offsets for OffsetRenderer

--- Voxel2Pixel/Render/OffsetRenderer.cs
+++ Voxel2Pixel/Render/OffsetRenderer.cs
@@ -10,13 +10,31 @@ namespace Voxel2Pixel.Render
 		public int ScaleX { get; set; } = 1;
 		public int ScaleY { get; set; } = 1;
 		#region IRectangleRenderer
-		public override void Rect(ushort x, ushort y, uint color, ushort sizeX = 1, ushort sizeY = 1) =>
+		public override void Rect(ushort x, ushort y, uint color, ushort sizeX = 1, ushort sizeY = 1)
+		{
+			int x2 = x * ScaleX + OffsetX,
+				y2 = y * ScaleY + OffsetY,
+				sizeX2 = sizeX * ScaleX,
+				sizeY2 = sizeY * ScaleY;
+			if (x2 < 0)
+			{
+				sizeX2 += x2;
+				x2 = 0;
+			}
+			if (y2 < 0)
+			{
+				sizeY2 += y2;
+				y2 = 0;
+			}
+			if (sizeX2 <= 0 || sizeY2 <= 0)
+				return;
 			RectangleRenderer.Rect(
-				x: (ushort)(x * ScaleX + OffsetX),
-				y: (ushort)(y * ScaleY + OffsetY),
+				x: (ushort)x2,
+				y: (ushort)y2,
 				color: color,
-				sizeX: (ushort)(sizeX * ScaleX),
-				sizeY: (ushort)(sizeY * ScaleY));
+				sizeX: (ushort)sizeX2,
+				sizeY: (ushort)sizeY2);
+		}
 		#endregion IRectangleRenderer
 	}
 }
