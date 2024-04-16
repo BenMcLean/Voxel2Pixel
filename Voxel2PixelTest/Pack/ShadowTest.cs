ad488e6981f8ab2732f1cb1201ef6a611368ec81
BenMcLean <mclean.ben@gmail.com>
2024-04-16 10:01:06 -0500

    made a shadow test

---
+++ Voxel2PixelTest/Pack/ShadowTest.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.ImageSharp;
+using Voxel2Pixel.Color;
+using Voxel2Pixel.Draw;
+using Voxel2Pixel.Interfaces;
+using Voxel2Pixel.Model;
+using Voxel2Pixel.Pack;
+using Voxel2Pixel.Render;
+using Xunit;
+
+namespace Voxel2PixelTest.Pack
+{
+	public class ShadowTest
+	{
+		[Fact]
+		public void Shadow()
+		{
+			VoxFileModel model = new(@"..\..\..\Sora.vox");
+			IVoxelColor voxelColor = new NaiveDimmer(model.Palette),
+				voxelShadow = new OneVoxelColor(0x00000088u);
+			Sprite2x sprite = new((ushort)(VoxelDraw.IsoWidth(model) << 1), VoxelDraw.IsoHeight(model))
+			{
+				VoxelColor = voxelColor,
+			};
+			OffsetRenderer offsetRenderer = new()
+			{
+				RectangleRenderer = sprite,
+				VoxelColor = voxelShadow,
+				OffsetY = model.SizeZ << 2,
+				ScaleX = 2,
+			};
+			VoxelDraw.IsoShadow(model, offsetRenderer);
+			VoxelDraw.Iso(model, sprite);
+			sprite.TransparentCrop().Png().SaveAsPng("ShadowTest.png");
+		}
+	}
+}
