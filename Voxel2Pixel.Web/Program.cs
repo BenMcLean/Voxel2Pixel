dec3863b9648dd821e8b0e750206adf1128da1bc
BenMcLean <mclean.ben@gmail.com>
2024-05-18 16:56:19 -0500

    Switched to MudBlazor

--- Voxel2Pixel.Web/Program.cs
+++ Voxel2Pixel.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace Voxel2Pixel.Web
 {
@@ -12,7 +13,7 @@ namespace Voxel2Pixel.Web
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+			builder.Services.AddMudServices();
 			await builder.Build().RunAsync();
 		}
 	}
