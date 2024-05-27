ad8c6eb59903816df09b9bf7aee0d847d2340f02
BenMcLean <mclean.ben@gmail.com>
2024-05-27 07:59:09 -0500

    moved test

--- Voxel2Pixel.Test/Draw/PixelDrawTest.cs
+++ Voxel2Pixel.Test/Draw/PixelDrawTest.cs
@@ -103,5 +103,59 @@ namespace Voxel2Pixel.Test.Draw
 			for (int x = 0; x < sprite.Texture.Length; x++)
 				Assert.Equal(sprite.Texture[x], stupid[x]);
 		}
+		[Fact]
+		public void Crop2ContentInfo()
+		{
+			byte[][][] bytes = TestData.Arch(46);
+			bytes[1][0][0] = 1;
+			Sprite sprite = new(
+					model: new ArrayModel(bytes),
+					voxelColor: new NaiveDimmer(TestData.RainbowPalette));
+			sprite.Texture.Crop2ContentInfo(
+				cutLeft: out ushort cutLeft,
+				cutTop: out ushort cutTop,
+				croppedWidth: out ushort croppedWidth,
+				croppedHeight: out ushort croppedHeight,
+				width: sprite.Width);
+			Crop2ContentInfo2(
+				texture: sprite.Texture,
+				cutLeft: out ushort cutLeft2,
+				cutTop: out ushort cutTop2,
+				croppedWidth: out ushort croppedWidth2,
+				croppedHeight: out ushort croppedHeight2,
+				width: sprite.Width);
+			Assert.Equal(cutLeft, cutLeft2);
+			Assert.Equal(cutTop, cutTop2);
+			Assert.Equal(croppedWidth, croppedWidth2);
+			Assert.Equal(croppedHeight, croppedHeight2);
+		}
+		protected static void Crop2ContentInfo2(byte[] texture, out ushort cutLeft, out ushort cutTop, out ushort croppedWidth, out ushort croppedHeight, ushort width = 0, byte threshold = DefaultTransparencyThreshold)
+		{
+			if (width < 1)
+				width = (ushort)Math.Sqrt(texture.Length >> 2);
+			uint[,] uints = texture.Texture2UInt2D(width);
+			int xSide = width << 2,
+				indexTop, indexBottom;
+			for (indexTop = 3; indexTop < texture.Length && texture[indexTop] < threshold; indexTop += 4) { }
+			cutTop = (ushort)(indexTop / xSide);
+			indexTop = (ushort)(cutTop * xSide);
+			for (indexBottom = texture.Length - 1; indexBottom > indexTop && texture[indexBottom] < threshold; indexBottom -= 4) { }
+			int cutBottom = indexBottom / xSide + 1;
+			croppedHeight = (ushort)(cutBottom - cutTop);
+			cutLeft = (ushort)(width - 1);
+			ushort cutRight = 0;
+			for (int y = cutTop; y < cutBottom; y++)
+			{
+				ushort left;
+				for (left = 0; left < cutLeft && (byte)uints[left, y] < threshold; left++) { }
+				if (left < cutLeft)
+					cutLeft = left;
+				ushort right;
+				for (right = (ushort)(width - 1); right > cutRight && (byte)uints[right, y] < threshold; right--) { }
+				if (right > cutRight)
+					cutRight = right;
+			}
+			croppedWidth = (ushort)(cutRight - cutLeft + 1);
+		}
 	}
 }
