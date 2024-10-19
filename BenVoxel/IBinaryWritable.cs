6dee877cb93ce021b0beac52858ea3e97d38f85b
BenMcLean <mclean.ben@gmail.com>
2024-10-19 09:09:09 -0500

    Spaces are so stupid. Forcing tabs.

--- BenVoxel/IBinaryWritable.cs
+++ BenVoxel/IBinaryWritable.cs
@@ -4,6 +4,6 @@ namespace BenVoxel;
 
 public interface IBinaryWritable
 {
-    void Write(Stream stream);
-    void Write(BinaryWriter writer);
+	void Write(Stream stream);
+	void Write(BinaryWriter writer);
 }
--- BenVoxel/IEditableModel.cs
+++ BenVoxel/IEditableModel.cs
@@ -2,5 +2,5 @@
 
 public interface IEditableModel : IModel
 {
-    new byte this[ushort x, ushort y, ushort z] { get; set; }
+	new byte this[ushort x, ushort y, ushort z] { get; set; }
 }
