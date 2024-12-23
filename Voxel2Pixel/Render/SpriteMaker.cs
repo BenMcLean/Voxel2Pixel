93ef8e2ad3916476938058876f47c43b6ce0a994
BenMcLean <mclean.ben@gmail.com>
2024-12-23 09:16:35 -0600

    Show Number of Sprites on web controls

--- Voxel2Pixel/Render/SpriteMaker.cs
+++ Voxel2Pixel/Render/SpriteMaker.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Voxel2Pixel.Color;
 using Voxel2Pixel.Draw;
 using Voxel2Pixel.Interfaces;
