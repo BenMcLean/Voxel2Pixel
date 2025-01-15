37cf400a2d8f6f9e48f5873ac63e980f4fe675d4
BenMcLean <mclean.ben@gmail.com>
2025-01-14 19:01:55 -0600

    added more extension methods to BenProgress

--- BenProgress/ExtensionMethods.cs
+++ BenProgress/ExtensionMethods.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,34 @@ public static class ExtensionMethods
 	#region PLINQ
 	/// <summary>
 	/// Parallelizes the execution of a Select query while preserving the order of the source sequence.
+	/// </summary>
+	public static async IAsyncEnumerable<TResult> ParallelizeWithProgress<TSource, TResult>(
+		this IEnumerable<TSource> source,
+		Func<TSource, CancellationToken?, IProgress<Progress>, Task<TResult>> selector,
+		[EnumeratorCancellation] CancellationToken cancellationToken = default,
+		IProgress<Progress> progress = null)
+	{
+		using ProgressTaskGroup<TResult> group = new(cancellationToken, progress);
+		group.Add(source.Select(item => new DelegateProgressTask<TResult>(
+			async (ct, p) => await selector(item, ct, p))));
+		while (group.HasPendingResults)
+			await foreach (TResult result in group.GetResultsAsync())
+				yield return result;
+	}
+	/// <summary>
+	/// Parallelizes the execution of a Select query while preserving the order of the source sequence.
+	/// </summary>
+	public static IAsyncEnumerable<TResult> ParallelizeWithProgress<TSource, TResult>(
+		this IEnumerable<TSource> source,
+		Func<TSource, TResult> selector,
+		CancellationToken cancellationToken = default,
+		IProgress<Progress> progress = null) =>
+		source.ParallelizeWithProgress(
+			(item, ct, p) => Task.FromResult(selector(item)),
+			cancellationToken,
+			progress);
+	/// <summary>
+	/// Simple PLINQ operation that maintains order without progress reporting
 	/// </summary>
 	public static List<TResult> Parallelize<TSource, TResult>(
 		this IEnumerable<TSource> source,
@@ -30,9 +58,8 @@ public static class ExtensionMethods
 		IProgress<Progress> progress = null)
 	{
 		using ProgressTaskGroup<TResult> group = new(cancellationToken, progress);
-		group.Add(source
-			.Select(item => new DelegateProgressTask<TResult>(
-				(ct, p) => selector(item, ct, p))));
+		group.Add(source.Select(item => new DelegateProgressTask<TResult>(
+			(ct, p) => selector(item, ct, p))));
 		while (group.HasPendingResults)
 			await foreach (TResult result in group.GetResultsAsync())
 				yield return result;
@@ -51,4 +78,18 @@ public static class ExtensionMethods
 			await foreach (TResult result in group.GetResultsAsync())
 				yield return result;
 	}
+	public static IAsyncEnumerable<TResult> ToResults<TSource, TResult>(
+		this IEnumerable<TSource> source,
+		Func<TSource, Task<TResult>> selector) =>
+		source.ToProgressResults(
+			(item, ct, p) => selector(item),
+			default,
+			null);
+	public static IAsyncEnumerable<TResult> ToResults<TSource, TResult>(
+		this IAsyncEnumerable<TSource> source,
+		Func<TSource, Task<TResult>> selector) =>
+		source.ToProgressResults(
+			(item, ct, p) => selector(item),
+			default,
+			null);
 }
