b758b489cd14d0f92afb2b5391abfd0f59655cfb
BenMcLean <mclean.ben@gmail.com>
2025-01-18 11:03:42 -0600

    Progress strings should use [StringSyntax(StringSyntaxAttribute.CompositeFormat)]

--- BenProgress/Progress.cs
+++ BenProgress/Progress.cs
@@ -1,34 +1,40 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using System.Threading;
 
 namespace BenProgress;
 
-public readonly record struct Progress(double? Double = null, string String = null)
+public readonly record struct Progress([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string String = null, double? Double = null)
 {
-	public Progress(string String = null, double? Double = null) : this(Double, String) { }
+	public Progress(
+		double? @double = null,
+		[StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format = null,
+		params object[] args) : this(String: string.Format(format, args), Double: @double) { }
 	public static async Task UpdateAsync(
 		CancellationToken? cancellationToken = null,
 		IProgress<Progress> progress = null,
 		double? @double = null,
-		string @string = null)
+		[StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format = null,
+		params object[] args)
 	{
-		progress?.Report(new Progress(@double, @string));
+		progress?.Report(new Progress(@double, format, args));
 		cancellationToken?.ThrowIfCancellationRequested();
 		await Task.Yield();
 	}
 	public static async Task<DateTimeOffset> UpdateAsync(
 		DateTimeOffset? lastCheck,
+		int minMillisecondsBetweenUpdates = 100,
 		CancellationToken? cancellationToken = null,
 		IProgress<Progress> progress = null,
 		double? @double = null,
-		string @string = null,
-		int minMillisecondsBetweenUpdates = 100)
+		[StringSyntax(StringSyntaxAttribute.CompositeFormat)] string format = null,
+		params object[] args)
 	{
 		DateTimeOffset now = DateTimeOffset.UtcNow;
 		if (lastCheck.HasValue && (now - lastCheck.Value).TotalMilliseconds < minMillisecondsBetweenUpdates)
 			return lastCheck.Value;
-		await UpdateAsync(cancellationToken, progress, @double, @string);
+		await UpdateAsync(cancellationToken, progress, @double, format, args);
 		return now;
 	}
 }
